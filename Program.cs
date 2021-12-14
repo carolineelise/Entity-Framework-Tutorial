d41f50aeb1823d533d47ee918b2288f2aa9b6dc9
carolineelise <90719451+carolineelise@users.noreply.github.com>
2021-12-14 14:54:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaledDbLib.Models;
+using SalesDbLib.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalesDbLib.Controllers;
+
+namespace ef_tutorial_codefirst
+{
+    class Program
+    {
+        async static void Main(string[] args)
+        {
+            var ordCtrl = new OrdersController();
+            var orders = await ordCtrlCrtl.GetAll();
+            orders.ForEach(o => Console.WriteLine($"{o.Descritpion} | {o.Customer.Name}"));
+
+            var cusCtrl = new CustomersController();
+            var customer = await cusCtrl.GetAll();
+            customer.ForEach(c => Console.WriteLine(c.Name));
+        }
+        
+    }
+}
